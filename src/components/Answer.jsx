120f14679e90199048a2ca192a82bbf74c5b2f19
= <=>
2019-12-27 23:46:27 -0800

    survey def from db

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -6,7 +6,9 @@ const Answer = (props) => {
 		'fontSize':'1.5em',
 		'padding':'1em',
 	};
+	// eslint-disable-next-line
 	const selectedIfFunc = Function('state',props.selectedIf);
+	// eslint-disable-next-line
 	const actionFunc = Function('state', props.action);
 
 	if (selectedIfFunc(props.state)) {
--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,17 +1,41 @@
-import React, { useState } from 'react';
-import Question from './Question';
+import React, { 
+  useEffect,
+  useState, 
+} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import * as babel from '@babel/standalone';
 import * as babelReact from '@babel/preset-react';
+import Question from './Question';
 
 const babelSettings = {
   parserOpts: {
     'allowReturnOutsideFunction': true
   },
   presets: [babelReact]
-}
+};
 
-const Survey = (props) => {
-  const [state, setState] = useState(props.initialState);
+const loadingSurvey = {
+  'name': 'loading...',
+  'initialState': {},
+  'questions': [],
+  'result': '',
+};
+
+const Survey = () => {
+  let { surveyId } = useParams();
+  const [surveyDef, setSurveyDef] = useState(loadingSurvey);
+  const [state, setState] = useState({});
+  useEffect(() => {
+    async function fetchSurvey() {
+      const result = await axios(
+        'http://localhost:3000/surveys/' + surveyId
+      );
+      setSurveyDef(result.data);
+      setState(result.data.initialState);
+    }
+    fetchSurvey();
+  }, [surveyId]);
 
   const surveyStyle = {
     'marginLeft':'auto',
@@ -34,19 +58,20 @@ const Survey = (props) => {
     'overflow':'hidden'
   };
 
-  const resultBabelTransformed = babel.transform(props.result, babelSettings).code;
+  const resultBabelTransformed = babel.transform(surveyDef.result, babelSettings).code;
+  // eslint-disable-next-line
   const resultFunction = Function('state','React', resultBabelTransformed);
 
   return (
     <div style={surveyStyle}>
       <div style={title}>
-        {props.name}
+        {surveyDef.name}
       </div>
       <div style={container}>
         <div style={left}>
           <h1>Questions</h1>
           {
-            props.questions.map((question) => Question({
+            surveyDef.questions.map((question) => Question({
               ...question,
               'state': state,
               'setState': setState
